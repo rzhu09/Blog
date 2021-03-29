8c1ac194472c284831fbc9fa77ac8ee0a7449546
Raymond <rzhu09@syr.edu>
2021-03-29 18:53:55 -0400

    My Django Blog App, First Comit

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!f:\important_desktop\everything_python\website\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
